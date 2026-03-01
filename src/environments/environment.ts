55e37d9941712ad027c17d60aeb39476835d552d
TruongDuyKhai <truongduykhai1012@gmail.com>
2026-03-01 19:57:54 +0700

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   discordId: '871329074046435338',
-  apiUrl: 'khaidevapi.onrender.com/discord/data/',
+  apiUrl: 'https://khaidevapi.onrender.com/discord/data/',
   webSocketUrl: 'wss://api.lanyard.rest/socket',
 };
